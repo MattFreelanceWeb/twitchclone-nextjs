8dad3af5cace27f69fc41231bb59ba4185c7fda8
MattFreelanceWeb <matt.freelance.web@gmail.com>
2023-03-23 15:10:39 +0100

    💄 change the style of the responsive header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,25 +1,27 @@
 import IonIcon from "@reacticons/ionicons";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {};
 
 function Header({}: Props) {
+  const [toggle, setToggle] = useState<boolean>(false)
+
   return (
-    <div className="bg-[#6441a4] h-16 fixed w-full z-50 text-white">
-      <nav className="w-full">
-        <ul className="flex w-full items-center ">
-          <li className="py-0 px-5">
+    <div className={`bg-[#6441a4] h-12 fixed w-full z-50 text-white overflow-hidden flex items-start justify-center p-1 duration-300 ${toggle? 'h-auto':'h-12'} md:h-16 p-0`}>
+      <nav className="w-full flex flex-col items-center md:flex-row">
+      <div className="py-0 px-5">
             <Link href="/">
               <span className="text-white cursor-pointer text-xl">
                 <IonIcon name="logo-twitch" size="large" />
               </span>
             </Link>
-          </li>
-          <li className="py-2 px-5 w-48 font-semibold text-xl hover:bg-[#19171c] rounded-lg duration-200 mx-4">
+          </div>
+        <ul className="flex flex-col w-full items-center  py-12 gap-8 md:flex-row md:py-0 md:gap-0">
+          <li onClick={()=>(setToggle(!toggle))} className="py-2 px-5 w-48 font-semibold text-xl hover:bg-[#19171c] rounded-lg duration-200 mx-4">
             <Link href="/topGames">Top Games</Link>
           </li>
-          <li className="py-2 px-5 w-48 font-semibold text-xl hover:bg-[#19171c] rounded-lg duration-200 mx-4">
+          <li onClick={()=>(setToggle(!toggle))} className="py-2 px-5 w-48 font-semibold text-xl hover:bg-[#19171c] rounded-lg duration-200 mx-4">
             <Link href="/topStreams">Top Streams</Link>
           </li>
           <li className=" px-5 mx-auto flex-shrink flex items-center justify-center h-16 w-full ">
@@ -40,7 +42,9 @@ function Header({}: Props) {
           </li>
         </ul>
       </nav>
-      <div className=" fixed top-0 right-2 cursor-pointer h-16 w-16 flex items-center justify-center md:hidden">
+      <div 
+      onClick={()=>(setToggle(!toggle))}
+      className=" fixed top-0 right-2 cursor-pointer h-12 w-12 flex items-center justify-center md:hidden">
         <span className="text-4xl text-white">
           <IonIcon name="menu" />
         </span>
